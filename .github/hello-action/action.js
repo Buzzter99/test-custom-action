f8db87d9bae2056ca7bff6f86a84ec618460e1d7
svetoslav.busarov <svetoslav.busarov@indeavr.com>
2025-04-24 16:59:51 +0300

    test

--- .github/hello-action/action.js
+++ .github/hello-action/action.js
@@ -1,8 +1,11 @@
 const core = require("@actions/core");
+const github = require("@actions/github");
 async function run() {
   try {
     const name = core.getInput("name");
+    const repo = github.context.repo;
     console.log(`Hello, ${name}!`);
+    console.log(`Context is: ${repo.owner}/${repo}`);
   } catch (error) {
     core.setFailed(error.message);
   }
